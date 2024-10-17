3f5c1ae0d509a9fbcd9ba7b4e9e6f8f0aa7906cd
BenjamingHatton <155596362+BenjamingHatton@users.noreply.github.com>
2024-10-17 14:11:35 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,21 +7,21 @@
 after_initialize do
   AdminUserSerializer.class_eval do
     def ip_address
-      if [1, 2].include?(self.id)
+      if [1, 2, 3].include?(self.id)
         'Hidden IP address'
       else
         object.ip_address || 'No IP Address Available'
       end
     end
     def registration_ip_address
-      if [1, 2].include?(self.id)
+      if [1, 2, 3].include?(self.id)
         'Hidden IP address'
       else
         object.registration_ip_address || 'No Registration IP Address Available'
       end
     end 
     def email
-      if [1, 2].include?(self.id)
+      if [1, 2, 3].include?(self.id)
         'Hidden Email address'
       else
         object.email || 'No Email Address Available'
